de7cdf667fc25a84524d8233442a1a65941eb86b
Drew Rudebusch <drewrudebusch@gmail.com>
2016-04-06 12:12:30 -0700

    Restrict drop to eligible spaces, log movements across board to object coordinates

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ $(document).ready(function () {
     var activeCol = 0;
     var moveRow = 0;
     var moveCol = 0;
-    var playerTurn = "red";
+    var redTurn = true;
     var activeSpace = "";
     var activePiece;
-    var moves =[];
+    var moves = [];
+    var revert = false;
 
     var getCoordinates = function(cell) {
         coordinates = cell.split('');
@@ -16,52 +17,75 @@ $(document).ready(function () {
         console.log(activeRow +" " + activeCol)
     };
 
-
     $('.piece').on('click', function(event) {
-        activeSpace = $(this).parent().attr("id");
-        activePiece = $(this);
+        // activeSpace = $(this).parent().attr("id");
+        // activePiece = $(this);
 
         // console.log(activeSpace)
         // getCoordinates(activeSpace);
 
-        if (activePiece.hasClass(playerTurn + "-piece")) {
-            if ($('.active-piece').length === 0 || $(this).hasClass('active-piece')) {
-                $(this).toggleClass('active-piece');
+        // if (activePiece.hasClass(playerTurn + "-piece")) {
+        //     if ($('.active-piece').length === 0 || $(this).hasClass('active-piece')) {
+        //         $(this).toggleClass('active-piece');
 
-            } else {
-                $(this).effect('shake', {direction: 'left', distance:3, times: 3}, 500);
-            }
-        } else {
-            $(this).effect('shake', {direction: 'left', distance:3, times: 3}, 500);
-        }
-        
-
+        //     } else {
+        //         $(this).effect('shake', {direction: 'left', distance:3, times: 3}, 500);
+        //     }
+        // } else {
+        //     $(this).effect('shake', {direction: 'left', distance:3, times: 3}, 500);
+        // }
     });
 
     $(function() {
         $( ".draggable" ).draggable({
-            snap: true,
-            snapMode: 'inner'
+            // snap: true,
+            // snapMode: 'inner'
+            revert: true,
+            revertDuration: 200
         });
-        $( ".droppable" ).droppable({
+        $( ".droppable" ).data('outside',1).droppable({
+            start: function(event, ui) {
+                activeSpace = $(data.helper).attr('id');
+                $(event.target).addClass('active-piece');
+
+            },
             // accept: '.draggable',
-            drop: function( event, ui ) {
-                console.log('piece dropped');
-                $(this).removeClass("border").removeClass("over");
-                var dropped = ui.draggable;
-                var droppedOn = $(this);
-                $(dropped).detach().css({top: 0, left: 0, 'z-index': 1000}).appendTo(droppedOn);
+            out        : function(){
+                $(this).data('outside',1);
             },
             over: function (event, ui) { 
-                console.log($(this));
-            }
-            stop: 
+                var hoverSpace = $(this).attr('id')
+                moves.push({"Row": hoverSpace[1], "Column": hoverSpace[3]});
+
+                console.log(moves);
+            },
+            drop: function( event, ui ) {
+                console.log('piece dropped');
+                var dropped = ui.helper;
+                var droppedOn = $(this);
+
+
+                if (!($(this).has('img').length)) {
+                    if(redTurn && !$(ui.helper).hasClass('crowned')) {
+                        $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
+                    }
+                }
+
+                //for (var i = 0; i < moves.length)
+
+
+                moves = [];
+            },
+            // stop: function( event, ui ) {
+                
+            // }
         });
     });
 
+
 });
 
-// (function($) {
+//(function($) {
 //     $.fn.drags = function(opt) {
 
 //         opt = $.extend({handle:"",cursor:"move"}, opt);
@@ -101,36 +125,5 @@ $(document).ready(function () {
 //         });
 
 //     }
-// })(jQuery);
+//})(jQuery);
 
-// var checkForEligibleMoves = function(cell) {
-
-//         if (activePiece.hasClass(playerTurn + "-piece") && (!(activePiece.hasClass('crowned')))) {
-//             if (playerTurn === 'red') {
-//                 if (activeCol > 1 && activeCol < 8 && activeRow < 8) {
-//                 console.log($('#r' + (activeRow + 1) + 'c' + (activeCol - 1)).find('img'));
-//                 if (($('#r' + (activeRow + 1) + 'c' + (activeCol - 1)).find('img').hasClass('black-piece') ||  
-//                     $('#r' + (activeRow + 1) + 'c' + (activeCol + 1)).find('img').hasClass('black-piece')) &&
-//                     ($('#r' + (activeRow + 2) + 'c' + (activeCol - 2)).find('img').hasClass('') ||
-//                     $('#r' + (activeRow + 2) + 'c' + (activeCol - 2)).find('img').hasClass(''))) { 
-//                         console.log('testing eligibility');
-//                     }
-//                 }
-                
-//         } else {
-//             return false;
-//         };
-//     };
-// };
-
-        // if ($('.active-piece').length === 0) {
-        //     $(this).toggleClass('active-piece');
-        // } else if ($(this).hasClass('active-piece')) {
-        //     $(this).toggleClass('active-piece');
-        // } else {
-        //     if(checkForEligibleMoves(activeSpace)) {
-
-        //     } else {
-                
-        //     }
-        // }
